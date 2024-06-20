f74791cf35b32626e0f4921f8ea3320ace138191
Dinuka Ekanayake <dinukaekanayake92@gmail.com>
2024-06-20 15:41:58 +0800

    init the project

---
+++ inventory-service/src/main/java/org/example/inventroyservice/InventroyServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.inventroyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventroyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventroyServiceApplication.class, args);
+    }
+
+}
